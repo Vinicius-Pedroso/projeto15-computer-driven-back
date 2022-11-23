e11edcd506667a1cdb3d87f1bbab121306be9c4c
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-23 19:20:41 -0300

    feat: Routes, middlewere & controlers

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { MongoClient } from 'mongodb';
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
-});
