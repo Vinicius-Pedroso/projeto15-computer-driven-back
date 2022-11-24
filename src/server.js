87c779d186cb6b1f2315b39749f6dee78cac7b7f
giovanni.padova <gpadova@belmare.com.br>
2022-11-24 02:05:52 -0300

    feat: schemas and routes directory adjusted

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import checkoutRoute from "./routes/checkoutRouter.js"
+import productsRoute from "./routes/productsRouter.js"
+import userRouter from "./routes/userRouter.js"
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(checkoutRoute)
+app.use(productsRoute)
+app.use(userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
