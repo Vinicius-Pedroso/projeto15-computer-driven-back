56e8c891b1200d5150a426865a3231965deea93f
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-27 15:41:05 -0300

    Back fix

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,7 +6,7 @@ import { validationSignUp } from '../middlewere/validationSignUp';
 const router = express.Router();
 router.post("/login", validationLogin , login)
 router.post("/Signup", validationSignUp ,signUp);
-router.get("/products", getTransaction);
-router.post("/cart", postTransaction)
-router.get("/cart", postTransaction)
+router.get("/", getProductions);
+router.post("/cart", postCart)
+router.get("/cart", postCart)
 export default router;
