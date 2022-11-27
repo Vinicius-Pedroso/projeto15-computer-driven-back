a24b3da39bfe9c98326062916521396f8a4cd14c
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-27 17:52:37 -0300

    Feat: Cart back-end

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { login, signUp } from '../controlers/authControler';
+import { postCartAlteration } from '../functions/handleCart';
 import { validationLogin } from '../middlewere/validationLogin';
 import { validationSignUp } from '../middlewere/validationSignUp';
 
@@ -7,6 +8,6 @@ const router = express.Router();
 router.post("/login", validationLogin , login)
 router.post("/Signup", validationSignUp ,signUp);
 router.get("/", getProductions);
-router.post("/cart", postCart)
+router.post("/cart", postCartAlteration)
 router.get("/cart", postCart)
 export default router;
